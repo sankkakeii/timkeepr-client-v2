5299acc59b17f9328955de954997538444bd2bcc
David Emeleogu <80433543+sankkakeii@users.noreply.github.com>
2024-05-17 17:34:10 +0100

    fix undefined email error v3

--- pages/_profile/analytics.js
+++ pages/_profile/analytics.js
@@ -64,7 +64,7 @@ export default function Analytics() {
         };
 
         fetchUser();
-        fetchClockInData();
+        // fetchClockInData();
     }, []);
 
     const handleLogOut = () => {
